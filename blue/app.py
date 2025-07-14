3f58c3e961d5ac9bee4bd89b5171f489930e85ba
arjungiridas <arjungiridas123456@gmail.com>
2025-07-14 12:47:05 +0530

    organized app into blue and green ver for deployment

--- blue/app.py
+++ blue/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello from your Dockerized Flask app deployed via EC2!"
+    return "<h1 style='color:blue'>Blue Version 🚀</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
