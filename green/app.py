a113b2097cc794eb3be592a689d2e4e1bc26f93c
arjungiridas <arjungiridas123456@gmail.com>
2025-07-15 13:22:38 +0530

    demo

--- green/app.py
+++ green/app.py
@@ -1,19 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 from prometheus_client import start_http_server, Counter
-import time
+import os
 
 app = Flask(__name__)
 
-# Define a counter metric
+# Prometheus metric
 REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests')
 
 @app.route('/')
-def hello():
+def home():
     REQUEST_COUNT.inc()
-    return "🚀 Hello from the UPDATED Flask app via Blue-Green Deployment(green)!"
+    version = os.getenv('VERSION', 'Unknown')
+    return render_template("index.html", version=version)
+
+@app.route('/version')
+def version():
+    return os.getenv('VERSION', 'Unknown')
+
+@app.route('/health')
+def health():
+    return "OK", 200
 
 if __name__ == '__main__':
     # Start Prometheus metrics server on port 8000
     start_http_server(8000)
     app.run(host='0.0.0.0', port=5000)
-
